e0400f609211fc45aa8ef658f978193818e5dd3b
Mark Frymire <mark.frymire@icloud.com>
2024-05-09 14:11:18 -0400

    Push cpp file into src folder

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nanobind-demo
 Proof that I can call C++ functions from Python using Nanobind
+
+## Build
+Install nanobind...  
+`
+python -m pip install nanobind
+`  
+
+Make sure CMake is installed, version 3.15+ is required. Then...  
+`
+cmake -S . -B build
+cmake --build build
+`
---
+++ src/my_ext.cpp
@@ -0,0 +1,12 @@
+#include <nanobind/nanobind.h>
+#include <iostream>
+
+int add(int a, int b) {
+    std::cout << "Whassup?" << std::endl;
+    return a + b;
+}
+
+NB_MODULE(my_ext, m) {
+    // Think of this as a main() that will run when importing this module into a python session.
+    m.def("add", &add);
+}
